84a945a6f9ef4f3882599a1a71c2e81bb020074c
kentromansB <bandong.fade@gmail.com>
2020-02-18 09:18:44 +0800

    express

---
+++ express/Express.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res){
+    res.end('Hello World!')
+})
+app.get('/home', function(req, res){
+    res.end('Hello World!')
+})
+app.listen(process.argv[2])

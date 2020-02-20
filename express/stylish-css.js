1639763aad904cfd6c676f4803fe9e8f5fd595aa
kentromansB <bandong.fade@gmail.com>
2020-02-20 18:33:12 +0800

    Updated Expressworks

--- express/stylish-css.js
+++ express/stylish-css.js
@@ -1,7 +1,8 @@
-var express =  require('express');
-var app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static(process.argv[3]));
 app.use(require('stylus').middleware(process.argv[3]));
+app.use(express.static(process.argv[3]));
 
-app.listen(process.argv[2]);
+
+app.listen(process.argv[2])
